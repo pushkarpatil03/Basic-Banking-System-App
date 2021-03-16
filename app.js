6d9d81be280894bbfccb97e5dd1aeaf02dd23283
Pushkar Patil <pushkarpatil03@gmail.com>
2021-03-16 23:02:27 +0530

    added start script

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const customers = require('./routes/customers');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-//const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/bank-sys';
-const dbUrl = 'mongodb://localhost:27017/bank-sys';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/bank-sys';
+// const dbUrl = 'mongodb://localhost:27017/bank-sys';
 
 mongoose.connect(dbUrl, { 
     useNewUrlParser: true, 
